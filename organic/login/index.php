115f1bb2df8b06e9c0c36ed4aed4d9735f2596c7
sundar447 <117264189+sundar447@users.noreply.github.com>
2022-11-02 14:11:40 +0530

    Add files via upload

---
+++ organic/login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <form class="form" method="post" action="logincheck.php">
+    <center><h1>Login Form</h1><br>
+    <label>username</label>
+    <input type="text" name="Username" ><br><br>
+    <label>Password</label>
+    <input type="password" name="Password"><br><br>
+    <button type="submit" >Login</button>
+    </center>
+    </form>
+
+</body>
+</html>
